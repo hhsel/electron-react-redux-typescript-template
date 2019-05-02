95bdaa11f76bea0e726870bee4c557accd33edfe
H.HSEL <26063868+hhsel@users.noreply.github.com>
2019-05-02 22:14:51 +0900

    Convert all files from js to ts/tsx
    Forgot to apply to About.js

--- src/renderer/components/AppBar.tsx
+++ src/renderer/components/AppBar.tsx
@@ -5,17 +5,22 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
 
+// See the link below to know why Link is not directly specified in component attribute, which can be done in JS.
+// https://stackoverflow.com/questions/53375964/using-a-link-component-with-listitem-and-typescript
 const Component = () => (
     <AppBar position="static">
         <Toolbar>
             <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
                 My Awesome Electron Application
             </Typography>
-            <Button component={Link} to="/" color="inherit">Home</Button>
-            <Button component={Link} to="/about/" color="inherit">About</Button>
+            <Button component={(props) => <Link to="/" {...props} />} color="inherit">
+                Home
+            </Button>
+            <Button component={(props) => <Link to="/about/" {...props} />} color="inherit">
+                About
+            </Button>
         </Toolbar>
     </AppBar>
 )
-
 
 export default Component
